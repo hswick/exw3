2615564aafce970c9007b86f4ac1e10efe00e5bf
hswick <hdswick@gmail.com>
2018-04-22 19:58:36 -0500

    Bumped version

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
   1. Add exw3 to your list of dependencies in mix.exs:
 
         def deps do
-          [{:exw3, "~> 0.0.1"}]
+          [{:exw3, "~> 0.0.2"}]
         end
 
   2. Ensure exw3 is started before your application:
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExW3.MixProject do
 
   def project do
     [app: :exw3,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.1-dev",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
