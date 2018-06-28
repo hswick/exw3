868dbab1d2762c242d528e2418ab852658b0e19d
hswick <hdswick@gmail.com>
2018-06-27 22:29:29 -0400

    Fix parity command for travis

--- travis_test.sh
+++ travis_test.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # This is for travis
-parity --chain dev --unlock=0x00a329c0648769a73afac7f9381e08fb43dbea72 --reseal-min-period 0 --password passfile 2>&1 &
+./parity --chain dev --unlock=0x00a329c0648769a73afac7f9381e08fb43dbea72 --reseal-min-period 0 --password passfile 2>&1 &
 sleep 10
 mix test
 sleep 10
