1be6299c581943005fbb01bbe56da1b908d13898
hswick <hdswick@gmail.com>
2018-10-09 17:42:57 -0500

    Bumped version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExW3.MixProject do
   def project do
     [
      app: :exw3,
-     version: "0.3.0",
+     version: "0.4.0",
      elixir: "~> 1.7.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
