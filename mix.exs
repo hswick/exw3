848cf1c7ae5cbcad957f24bc5e6dfd37a7ce6c3c
hswick <hdswick@gmail.com>
2020-10-21 19:33:07 -0500

    Bump elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExW3.MixProject do
     [
      app: :exw3,
      version: "0.4.4",
-     elixir: "~> 1.7.2",
+     elixir: "~> 1.10",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
