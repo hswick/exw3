cf9cc0c6700a2590bf53da7f479e9adadaab3b16
Alex <alex+git@fremantle.io>
2020-11-26 10:29:58 -0800

    Install rust in TravisCI

--- travis_install.sh
+++ travis_install.sh
@@ -1,6 +1,9 @@
-# just to be safe
-echo > passfile
+# Install Rust for ex_keccack NIF
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
 
+# Install parity blockchain for testing
+echo > passfile # just to be safe
 
 wget https://releases.parity.io/ethereum/v1.8.5/x86_64-unknown-linux-gnu/parity
 
@@ -11,4 +14,4 @@ echo > passfile
 
 PARITY_PID=$!
 sleep 10
-kill -9 $(lsof -t -i:8545)
+kill -9 $(lsof -t -i:8545) # cleanup old zombie instances
