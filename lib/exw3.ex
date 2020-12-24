d6c6ac947315b299999af03b4e51a4f8ee652108
Alex <alex+git@fremantle.io>
2020-12-23 23:25:22 -0800

    Extract client module
    - Add test coverage for default usage + url override
    - Improve test UX with docker-compose
    - Add test instructions to README

--- lib/exw3.ex
+++ lib/exw3.ex
@@ -131,18 +131,10 @@ defmodule ExW3 do
     to_checksum_address(address) == address
   end
 
-  defp call_client(method_name, arguments \\ []) do
-    case get_client_type() do
-      :http -> apply(Ethereumex.HttpClient, method_name, arguments)
-      :ipc -> apply(Ethereumex.IpcClient, method_name, arguments)
-      _ -> {:error, :invalid_client_type}
-    end
-  end
-
   @spec accounts() :: list()
   @doc "returns all available accounts"
   def accounts do
-    case call_client(:eth_accounts) do
+    case ExW3.Client.call_client(:eth_accounts) do
       {:ok, accounts} -> accounts
       err -> err
     end
@@ -159,7 +151,7 @@ defmodule ExW3 do
   @spec block_number() :: integer()
   @doc "Returns the current block number"
   def block_number do
-    case call_client(:eth_block_number) do
+    case ExW3.Client.call_client(:eth_block_number) do
       {:ok, block_number} ->
         block_number |> to_decimal
 
@@ -171,7 +163,7 @@ defmodule ExW3 do
   @spec balance(binary()) :: integer() | {:error, any()}
   @doc "Returns current balance of account"
   def balance(account) do
-    case call_client(:eth_get_balance, [account]) do
+    case ExW3.Client.call_client(:eth_get_balance, [account]) do
       {:ok, balance} ->
         balance |> to_decimal
 
@@ -188,7 +180,7 @@ defmodule ExW3 do
   @spec tx_receipt(binary()) :: {:ok, map()} | {:error, any()}
   @doc "Returns transaction receipt for specified transaction hash(id)"
   def tx_receipt(tx_hash) do
-    case call_client(:eth_get_transaction_receipt, [tx_hash]) do
+    case ExW3.Client.call_client(:eth_get_transaction_receipt, [tx_hash]) do
       {:ok, nil} ->
         {:error, :not_mined}
 
@@ -205,7 +197,7 @@ defmodule ExW3 do
   @spec block(integer()) :: any() | {:error, any()}
   @doc "Returns block data for specified block number"
   def block(block_number) do
-    case call_client(:eth_get_block_by_number, [block_number, true]) do
+    case ExW3.Client.call_client(:eth_get_block_by_number, [block_number, true]) do
       {:ok, block} -> block
       err -> err
     end
@@ -214,7 +206,7 @@ defmodule ExW3 do
   @spec new_filter(map()) :: binary() | {:error, any()}
   @doc "Creates a new filter, returns filter id. For more sophisticated use, prefer ExW3.Contract.filter."
   def new_filter(map) do
-    case call_client(:eth_new_filter, [map]) do
+    case ExW3.Client.call_client(:eth_new_filter, [map]) do
       {:ok, filter_id} -> filter_id
       err -> err
     end
@@ -223,7 +215,7 @@ defmodule ExW3 do
   @spec get_filter_changes(binary()) :: any()
   @doc "Gets event changes (logs) by filter. Unlike ExW3.Contract.get_filter_changes it does not return the data in a formatted way"
   def get_filter_changes(filter_id) do
-    case call_client(:eth_get_filter_changes, [filter_id]) do
+    case ExW3.Client.call_client(:eth_get_filter_changes, [filter_id]) do
       {:ok, changes} -> changes
       err -> err
     end
@@ -232,7 +224,7 @@ defmodule ExW3 do
   @spec uninstall_filter(binary()) :: boolean() | {:error, any()}
   @doc "Uninstalls filter from the ethereum node"
   def uninstall_filter(filter_id) do
-    case call_client(:eth_uninstall_filter, [filter_id]) do
+    case ExW3.Client.call_client(:eth_uninstall_filter, [filter_id]) do
       {:ok, result} -> result
       err -> err
     end
@@ -242,57 +234,57 @@ defmodule ExW3 do
   @doc "Mines number of blocks specified. Default is 1"
   def mine(num_blocks \\ 1) do
     for _ <- 0..(num_blocks - 1) do
-      call_client(:request, ["evm_mine", [], []])
+      ExW3.Client.call_client(:request, ["evm_mine", [], []])
     end
   end
 
   @spec personal_list_accounts(list()) :: {:ok, list()} | {:error, any()}
   @doc "Using the personal api, returns list of accounts."
   def personal_list_accounts(opts \\ []) do
-    call_client(:request, ["personal_listAccounts", [], opts])
+    ExW3.Client.call_client(:request, ["personal_listAccounts", [], opts])
   end
 
   @spec personal_new_account(binary(), list()) :: {:ok, binary()} | {:error, any()}
   @doc "Using the personal api, this method creates a new account with the passphrase, and returns new account address."
   def personal_new_account(password, opts \\ []) do
-    call_client(:request, ["personal_newAccount", [password], opts])
+    ExW3.Client.call_client(:request, ["personal_newAccount", [password], opts])
   end
 
   @spec personal_unlock_account(binary(), list()) :: {:ok, boolean()} | {:error, any()}
   @doc "Using the personal api, this method unlocks account using the passphrase provided, and returns a boolean."
   ### E.g. ExW3.personal_unlock_account(["0x1234","Password",30], [])
   def personal_unlock_account(params, opts \\ []) do
-    call_client(:request, ["personal_unlockAccount", params, opts])
+    ExW3.Client.call_client(:request, ["personal_unlockAccount", params, opts])
   end
 
   @spec personal_send_transaction(map(), binary(), list()) :: {:ok, binary()} | {:error, any()}
   @doc "Using the personal api, this method sends a transaction and signs it in one call, and returns a transaction id hash."
   def personal_send_transaction(param_map, passphrase, opts \\ []) do
-    call_client(:request, ["personal_sendTransaction", [param_map, passphrase], opts])
+    ExW3.Client.call_client(:request, ["personal_sendTransaction", [param_map, passphrase], opts])
   end
 
   @spec personal_sign_transaction(map(), binary(), list()) :: {:ok, map()} | {:error, any()}
   @doc "Using the personal api, this method signs a transaction, and returns the signed transaction."
   def personal_sign_transaction(param_map, passphrase, opts \\ []) do
-    call_client(:request, ["personal_signTransaction", [param_map, passphrase], opts])
+    ExW3.Client.call_client(:request, ["personal_signTransaction", [param_map, passphrase], opts])
   end
 
   @spec personal_sign(binary(), binary(), binary(), list()) :: {:ok, binary()} | {:error, any()}
   @doc "Using the personal api, this method calculates an Ethereum specific signature, and returns that signature."
   def personal_sign(data, address, passphrase, opts \\ []) do
-    call_client(:request, ["personal_sign", [data, address, passphrase], opts])
+    ExW3.Client.call_client(:request, ["personal_sign", [data, address, passphrase], opts])
   end
 
   @spec personal_ec_recover(binary(), binary(), []) :: {:ok, binary()} | {:error, any()}
   @doc "Using the personal api, this method returns the address associated with the private key that was used to calculate the signature with personal_sign."
   def personal_ec_recover(data0, data1, opts \\ []) do
-    call_client(:request, ["personal_ecRecover", [data0, data1], opts])
+    ExW3.Client.call_client(:request, ["personal_ecRecover", [data0, data1], opts])
   end
 
   @spec eth_sign(binary(), binary(), list()) :: {:ok, binary()} | {:error, any()}
   @doc "Calculates an Ethereum specific signature and signs the data provided, using the accounts private key"
   def eth_sign(data0, data1, opts \\ []) do
-    call_client(:request, ["eth_sign", [data0, data1], opts])
+    ExW3.Client.call_client(:request, ["eth_sign", [data0, data1], opts])
   end
 
   @spec encode_event(binary()) :: binary()
@@ -306,13 +298,13 @@ defmodule ExW3 do
   @spec eth_call(list()) :: any()
   @doc "Simple eth_call to client. Recommended to use ExW3.Contract.call instead."
   def eth_call(arguments) do
-    call_client(:eth_call, arguments)
+    ExW3.Client.call_client(:eth_call, arguments)
   end
 
   @spec eth_send(list()) :: any()
   @doc "Simple eth_send_transaction. Recommended to use ExW3.Contract.send instead."
   def eth_send(arguments) do
-    call_client(:eth_send_transaction, arguments)
+    ExW3.Client.call_client(:eth_send_transaction, arguments)
   end
 
   @spec decode_event(binary(), binary()) :: any()
