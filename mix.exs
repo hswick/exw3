e5d76926ea562c21128a50966f9631e5584ae8fc
William Leong <willjleong@gmail.com>
2021-09-26 16:14:21 -0700

    lower elixir version to pass CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExW3.MixProject do
     [
       app: :ex_w3,
       version: "0.6.1",
-      elixir: "~> 1.11",
+      elixir: "~> 1.10",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
