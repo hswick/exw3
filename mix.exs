06731fbeb1a3c4d7fa6d04f534d98b7b6c6868df
alex <alex+git@fremantle.io>
2021-11-17 15:14:07 -0800

    Release v0.6.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExW3.MixProject do
   def project do
     [
       app: :exw3,
-      version: "0.6.0",
+      version: "0.6.1",
       elixir: "~> 1.10",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
