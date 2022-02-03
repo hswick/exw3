710cfdf7b5140d92de06ffe5b7cd669292d3f51b
alex <alex+git@fremantle.io>
2022-02-02 23:53:47 -0800

    Add Address struct with util helpers

--- test/exw3_test.exs
+++ test/exw3_test.exs
@@ -332,7 +332,7 @@ defmodule ExW3Test do
 
     {:ok, same_address} = ExW3.Contract.call(:AddressTester, :get, [formatted_address])
 
-    assert ExW3.Utils.to_address(same_address) == Enum.at(context[:accounts], 0)
+    assert %ExW3.Address{bytes: same_address} |> ExW3.Address.to_hex() == Enum.at(context[:accounts], 0)
   end
 
   test "returns proper error messages at contract deployment", context do
